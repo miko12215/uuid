6f012eaea57ab16bf7d933faa95176ef63a3f7c3
Mikolaj Wancicki <miko12215@gmail.com>
2022-12-27 11:10:13 +0100

    test

--- src/test/java/com/uuid/uuid/UuidApplicationTests.java
+++ src/test/java/com/uuid/uuid/UuidApplicationTests.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.util.Assert;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @ActiveProfiles("test")
 @SpringBootTest
@@ -13,7 +16,7 @@ class UuidApplicationTests {
 	void contextLoads() {
 
 
-sdfsdfsdf
+		Assert.isTrue(false);
 
 
 	}
